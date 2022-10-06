61975269d53b4deb09b061293aca472ea43543a5
안치산 <chisan2001@gmail.com>
2022-10-06 22:31:47 +0900

    Time: 412 ms (24.50%), Space: 218.4 MB (47.43%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,25 +1,41 @@
 class TimeMap {
 
-    private HashMap<String, TreeMap<Integer, String>> valuesOfKey;
+    private class ValueWithTimestamp {
+        public String value;
+        int timestamp;
+
+        public ValueWithTimestamp(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    private Map<String, List<ValueWithTimestamp>> valuesOfKey;
 
     public TimeMap() {
         valuesOfKey = new HashMap<>();
     }
 
     public void set(String key, String value, int timestamp) {
-        if(!valuesOfKey.containsKey(key)) valuesOfKey.put(key, new TreeMap<>());
-        this.valuesOfKey.get(key).put(timestamp, value);
+        List<ValueWithTimestamp> values = this.valuesOfKey.getOrDefault(key, new ArrayList<>());
+        values.add(new ValueWithTimestamp(value, timestamp));
+        valuesOfKey.put(key, values);
     }
 
     public String get(String key, int timestamp) {
         if(!valuesOfKey.containsKey(key)) return "";
 
-        TreeMap<Integer, String> treeMap = this.valuesOfKey.get(key);
+        List<ValueWithTimestamp> values = this.valuesOfKey.get(key);
 
-        Integer floor = treeMap.floorKey(timestamp);
-        
-        if(floor == null) return "";
+        if(values.get(0).timestamp > timestamp) return "";
 
-        return treeMap.get(floor);
+        int L = 0, R = values.size() - 1;
+        while(L < R) {
+            int mid = (L + R + 1) / 2;
+            if(values.get(mid).timestamp <= timestamp) L = mid;
+            else R = mid - 1;
+        }
+
+        return values.get(R).value;
     }
 }
