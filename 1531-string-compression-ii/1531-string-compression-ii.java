2142e3e6de39bb82d476126bd46cf1a088bee9f1
안치산 <chisan2001@gmail.com>
2022-10-15 21:44:26 +0900

    Time: 130 ms (14.94%), Space: 43.4 MB (32.18%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+
+    int[][] dp;
+
+    void init(String s, int k) {
+        this.dp = new int[s.length() + 1][k + 1];
+        for(int[] row : dp) Arrays.fill(row, 101);
+        dp[0][0] = 0;
+    }
+
+    int calLenOfCompressedString(int sameCharCnt) {
+        if(sameCharCnt >= 100) return 4;
+        else if(sameCharCnt >= 10) return 3;
+        else if(sameCharCnt > 1) return 2;
+        return 1;
+    }
+
+    public int getLengthOfOptimalCompression(String s, int maxDelCnt) {
+        init(s, maxDelCnt);
+
+        for (int R = 1; R <= s.length(); R++) {
+            for (int delCnt = 0; delCnt <= maxDelCnt; delCnt++) {
+                // keep s[lastIdx]
+                int sameCharCnt = 0, differentCharCnt = 0;
+                for (int L = R; L >= 1; L--) {
+                    if (s.charAt(L-1) == s.charAt(R-1)) sameCharCnt++;
+                    else differentCharCnt++;
+
+                    if(differentCharCnt > delCnt) continue;
+
+                    dp[R][delCnt] = Math.min(
+                            dp[R][delCnt],
+                            dp[L-1][delCnt - differentCharCnt] + calLenOfCompressedString(sameCharCnt)
+                    );
+                }
+
+                // delete s[lastIdx]
+                if(delCnt == 0) continue;
+                dp[R][delCnt] = Math.min(dp[R][delCnt], dp[R - 1][delCnt - 1]);
+            }
+        }
+        return dp[s.length()][maxDelCnt];
+    }
+}
