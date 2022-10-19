e3790bb07e69801d65ade7d8df46bde98533dabb
안치산 <chisan2001@gmail.com>
2022-10-19 17:26:48 +0900

    Time: 409 ms (62.07%), Space: 41.1 MB (49.14%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.kt
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.kt
@@ -2,25 +2,36 @@ import java.util.*
 
 class Solution {
     fun topKFrequent(words: Array<String>, k: Int): List<String> {
-        val frequencyOfWords  = mutableMapOf<String, Int>()
-        for (word in words) {
-            val frequencyOfCurWord = frequencyOfWords.getOrDefault(word, 0)
-            frequencyOfWords[word] = frequencyOfCurWord + 1
-        }
+        val frequencyOfWords = calFrequency(words)
+        return getTopKFrequentFrom(frequencyOfWords, k)
+    }
 
-        val pq = PriorityQueue<Pair<Int, String>> {a, b ->
-            if(a.first == b.first) a.second.compareTo(b.second)
-            else b.first - a.first
-        }
-
-        for (word in frequencyOfWords.keys) {
-            pq.add(Pair(frequencyOfWords.getOrDefault(word, 0), word))
-        }
-
+    private fun getTopKFrequentFrom(frequencyOfWords: MutableMap<String, Int>, k: Int): MutableList<String> {
+        val pq = convertMapToPriorityQueue(frequencyOfWords)
         val answer = mutableListOf<String>()
         for (i in 1..k) {
             answer.add(pq.poll().second)
         }
         return answer
+    }
+
+    private fun convertMapToPriorityQueue(frequencyOfWords: MutableMap<String, Int>): PriorityQueue<Pair<Int, String>> {
+        val pq = PriorityQueue<Pair<Int, String>> { a, b ->
+            if (a.first == b.first) a.second.compareTo(b.second)
+            else b.first - a.first
+        }
+        for (word in frequencyOfWords.keys) {
+            pq.add(Pair(frequencyOfWords.getOrDefault(word, 0), word))
+        }
+        return pq
+    }
+
+    private fun calFrequency(words: Array<String>): MutableMap<String, Int> {
+        val frequencyOfWords = mutableMapOf<String, Int>()
+        for (word in words) {
+            val frequencyOfCurWord = frequencyOfWords.getOrDefault(word, 0)
+            frequencyOfWords[word] = frequencyOfCurWord + 1
+        }
+        return frequencyOfWords
     }
 }
