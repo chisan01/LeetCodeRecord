f6fe214af229b4a6d581cd65fd615f78e3874fa6
안치산 <chisan2001@gmail.com>
2022-10-21 11:16:47 +0900

    Time: 1109 ms (21.14%), Space: 77 MB (22.76%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.kt
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.kt
@@ -2,13 +2,12 @@ import kotlin.math.min
 
 class Solution {
     fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val subNumsLengthOfK = mutableSetOf<Int>()
         for (i in nums.indices) {
-
-            val curNum = nums[i]
-
-            for (j in i + 1..min(nums.lastIndex, i + k)) {
-                if(curNum == nums[j]) return true
-            }
+            val num = nums[i]
+            if(i > k) subNumsLengthOfK.remove(nums[i - k - 1])
+            if(subNumsLengthOfK.contains(num)) return true
+            subNumsLengthOfK.add(num)
         }
         return false
     }
