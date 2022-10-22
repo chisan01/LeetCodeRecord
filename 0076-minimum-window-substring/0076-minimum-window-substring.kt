622f217794de8006b15ed04840f2129b728c1058
안치산 <chisan2001@gmail.com>
2022-10-22 21:34:31 +0900

    Time: 492 ms (65.73%), Space: 43.4 MB (71.33%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.kt
@@ -0,0 +1,45 @@
+class Solution {
+
+    fun minWindow(s: String, t: String): String {
+        if(s.length < t.length || s.isEmpty()) return "";
+
+        val map = mutableMapOf<Char, Int>()
+        for (ch in t) {
+            map[ch] = map.getOrDefault(ch, 0) + 1
+        }
+
+        var left = 0
+        var minLeft = 0
+        var minLen = s.length + 1
+        var numOfCharInT = 0
+
+        for (right in s.indices) {
+            val curCh = s[right]
+            if (!map.containsKey(curCh)) continue
+
+            // is s.substring(left, right+1) window substring?
+            map[curCh] = map[curCh]!! - 1
+            if (map[curCh]!! >= 0) numOfCharInT++
+
+            // find min value of left
+            while (numOfCharInT == t.length) {
+                // update minRight, minLeft
+                if(right - left + 1 < minLen) {
+                    minLeft = left
+                    minLen = right - left + 1
+                }
+                
+                if (map.containsKey(s[left])) {
+                    map[s[left]] = map[s[left]]!! + 1
+                    if(map[s[left]]!! > 0) {
+                        numOfCharInT--
+                    }
+                }
+                left++
+            }
+        }
+
+        if (minLen > s.length) return ""
+        return s.substring(minLeft, minLeft + minLen)
+    }
+}
