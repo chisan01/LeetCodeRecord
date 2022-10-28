e89c2cb0d934eca0c58611eca68826a74af2b40f
안치산 <chisan2001@gmail.com>
2022-10-28 11:45:05 +0900

    Time: 423 ms (90.53%), Space: 43.3 MB (95.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.kt
+++ 0049-group-anagrams/0049-group-anagrams.kt
@@ -2,24 +2,20 @@ import java.util.PriorityQueue
 
 class Solution {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-
         val map = mutableMapOf<String, MutableList<String>>()
         for (str in strs) {
             val key = str.compact()
-            map[key] = map.getOrDefault(key, mutableListOf())
-                .apply { add(str) }
+            map[key] = map.getOrDefault(key, mutableListOf()).apply { add(str) }
         }
-
-        val result = mutableListOf<List<String>>()
-        for(values in map.values) {
-            result.add(values)
-        }
-        return result
+        return map.values.toList()
     }
 }
 
 fun String.compact(): String {
     val cnts = MutableList<Int>('z' - 'a' + 1) { 0 }
     forEach { cnts[it - 'a']++ }
-    return cnts.withIndex().joinToString(" ")
+    return cnts.withIndex().joinToString(" ") {
+        if(it.value == 0) ""
+        else "" + ('a' + it.index) + it.value
+    }
 }
