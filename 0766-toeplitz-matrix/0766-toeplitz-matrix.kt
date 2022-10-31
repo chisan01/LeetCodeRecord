dae905c5104ffff1c7f4e0808c51ebe3298ccc24
안치산 <chisan2001@gmail.com>
2022-10-31 11:02:59 +0900

    Time: 293 ms (85.71%), Space: 44.6 MB (67.86%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.kt
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.kt
@@ -5,8 +5,7 @@ class Solution {
         val linkedList = LinkedList<Int>()
         matrix[0].forEach { linkedList.add(it) }
         for(i in 1..matrix.lastIndex) {
-            matrix[i].forEachIndexed {
-                j, item ->
+            matrix[i].forEachIndexed { j, item ->
                 if(j == 0) linkedList.addFirst(item)
                 else if(linkedList[j] != item) return false
             }
