43a9cc96aef26df0a9cd68d1fa00dad76cf48e86
안치산 <chisan2001@gmail.com>
2022-10-31 11:02:43 +0900

    Time: 255 ms (89.29%), Space: 43.7 MB (75.00%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+class Solution {
+    fun isToeplitzMatrix(matrix: Array<IntArray>): Boolean {
+        val linkedList = LinkedList<Int>()
+        matrix[0].forEach { linkedList.add(it) }
+        for(i in 1..matrix.lastIndex) {
+            matrix[i].forEachIndexed {
+                j, item ->
+                if(j == 0) linkedList.addFirst(item)
+                else if(linkedList[j] != item) return false
+            }
+            linkedList.removeLast()
+        }
+        return true
+    }
+}
