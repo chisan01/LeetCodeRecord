4018a11068359bed0fe852f44da6198ac488b93c
안치산 <chisan2001@gmail.com>
2022-11-18 11:12:14 +0900

    Time: 249 ms (60.53%), Space: 34.3 MB (31.58%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun isUgly(n: Int): Boolean {
+        if(n <= 0) return false
+        var num = n
+        while(num > 1) {
+            if (num % 2 == 0) {
+                num /= 2
+                continue;
+            }
+            if (num % 3 == 0) {
+                num /= 3
+                continue;
+            }
+            if (num % 5 == 0) {
+                num /= 5
+                continue;
+            }
+            return false
+        }
+        return true
+    }
+}
