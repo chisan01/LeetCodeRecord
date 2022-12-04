05443e230d80ea8a7917ddd4da6078f9b9c6135b
안치산 <chisan2001@gmail.com>
2022-12-04 11:52:23 +0900

    Time: 4 ms (51.06%), Space: 47.9 MB (36.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,6 +1,7 @@
 class Solution {
 
     private void concat(ListNode prev, ListNode other, int carry) {
+        if(other == null) return;
         while (other != null) {
             int sum = other.val + carry;
             ListNode cur = new ListNode(sum % 10, null);
@@ -15,32 +16,26 @@ class Solution {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int sum = l1.val + l2.val;
-        ListNode root = new ListNode(sum % 10, null);
-        int carry = sum / 10;
-        l1 = l1.next;
-        l2 = l2.next;
-
-        ListNode prev = root;
+        ListNode root = null;
+        ListNode prev = null;
+        int carry = 0;
+        
         while (l1 != null && l2 != null) {
-            sum = l1.val + l2.val + carry;
+            int sum = l1.val + l2.val + carry;
             ListNode cur = new ListNode(sum % 10, null);
             carry = sum / 10;
             l1 = l1.next;
             l2 = l2.next;
-            prev.next = cur;
+            if(prev != null) prev.next = cur;
+            else root = cur;
             prev = cur;
         }
 
         if(l1 == null && l2 == null && carry != 0) {
             prev.next = new ListNode(carry, null);
         }
-        else if(l1 != null) {
-            concat(prev, l1, carry);
-        }
-        else {
-            concat(prev, l2, carry);
-        }
+        concat(prev, l1, carry);
+        concat(prev, l2, carry);
         return root;
     }
 }
