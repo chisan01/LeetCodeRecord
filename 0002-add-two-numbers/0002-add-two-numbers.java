a14b817ee184abf516de9e6ecbf59bb1bfe1e3e2
안치산 <chisan2001@gmail.com>
2022-12-04 11:39:32 +0900

    Time: 4 ms (51.06%), Space: 47.8 MB (44.68%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,44 +1,46 @@
 class Solution {
+
+    private void concat(ListNode prev, ListNode other, int carry) {
+        while (other != null) {
+            int sum = other.val + carry;
+            ListNode cur = new ListNode(sum % 10, null);
+            carry = sum / 10;
+            other = other.next;
+            prev.next = cur;
+            prev = cur;
+        }
+        if(carry != 0) {
+            prev.next = new ListNode(carry, null);
+        }
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int sum = l1.val + l2.val;
         ListNode root = new ListNode(sum % 10, null);
-        int sumOfTen = sum / 10;
+        int carry = sum / 10;
         l1 = l1.next;
         l2 = l2.next;
 
         ListNode prev = root;
         while (l1 != null && l2 != null) {
-            sum = l1.val + l2.val + sumOfTen;
+            sum = l1.val + l2.val + carry;
             ListNode cur = new ListNode(sum % 10, null);
-            sumOfTen = sum / 10;
+            carry = sum / 10;
             l1 = l1.next;
             l2 = l2.next;
             prev.next = cur;
             prev = cur;
         }
 
-        while(l1 != null) {
-            sum = l1.val + sumOfTen;
-            ListNode cur = new ListNode(sum % 10, null);
-            sumOfTen = sum / 10;
-            l1 = l1.next;
-            prev.next = cur;
-            prev = cur;
+        if(l1 == null && l2 == null && carry != 0) {
+            prev.next = new ListNode(carry, null);
         }
-
-        while(l2 != null) {
-            sum = l2.val + sumOfTen;
-            ListNode cur = new ListNode(sum % 10, null);
-            sumOfTen = sum / 10;
-            l2 = l2.next;
-            prev.next = cur;
-            prev = cur;
+        else if(l1 != null) {
+            concat(prev, l1, carry);
         }
-
-        if(sumOfTen != 0) {
-            prev.next = new ListNode(sumOfTen, null);
+        else {
+            concat(prev, l2, carry);
         }
-
         return root;
     }
 }
